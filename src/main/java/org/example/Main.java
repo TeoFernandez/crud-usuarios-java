45e07b07cb6b46938d21d527a0dbced585372114
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-27 20:05:14 -0300

    Termine de programar el main y ahora el sistema está listo para hacer pruebas de funcionamiento

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,31 @@
 package org.example;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        UsuarioDAO dao = new UsuarioDAO();
+
+        //Crear nuevo usuario
+        Usuario nuevo = new Usuario("Juan Pérez", "juan@gmail.com");
+        dao.agregarUsuario(nuevo);
+
+        //Mostrar todos
+        List<Usuario> usuarios = dao.obtenerUsuarios();
+        System.out.println("\nLista de usuarios:");
+
+        for(Usuario u : usuarios){
+            System.out.println(u.getId() + " - " +
+                                u.getNombre() + " - " +
+                                u.getEmail() + " - ");
+        }
+
+        //Actualizar usuario (por ejemplo el ID 1)
+        Usuario actualizado = new Usuario(1,"Juan Actualizado", "nuevo@gmail.com");
+        dao.actualizarUsuarios(actualizado);
+
+        //Eliminar usuario (por ejemplo, el ID 2)
+        dao.eliminarUsuario(2);
     }
 }
