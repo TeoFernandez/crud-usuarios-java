79a6498c3480ab713dc4423474066dbafccf4c25
Teo Fernández <81999491+TeoFernandez@users.noreply.github.com>
2025-05-27 19:51:11 -0300

    Agregando metodos en UsuarioDAO

--- src/main/java/org/example/Conexion.java
+++ src/main/java/org/example/Conexion.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
+    private static final String url= "jdbc:mysql://localhost:3306/jpaprueba";
+    private static final String usuario = "root";
+    private static final String contrasenia = "";
+
+    public static Connection conectar() {
+        try {
+            return DriverManager.getConnection(url, usuario, contrasenia);
+        } catch (SQLException e) {
+            System.out.println("Error al conectar la base de datos: ");
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
--- src/main/java/org/example/UsuarioDAO.java
+++ src/main/java/org/example/UsuarioDAO.java
@@ -1,4 +1,77 @@
 package org.example;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UsuarioDAO {
+
+    public void agregarUsuario(Usuario usuario){
+        String sql = "INSERT INTO usuarios (nombre, email) VALUES  (?, ?)";
+
+        try(Connection conn = Conexion.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, usuario.getNombre());
+            stmt.setString(2, usuario.getEmail());
+            stmt.executeUpdate();
+            System.out.println("Usuario agregado correctamente.");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Usuario> obtenerUsuarios(){
+        List<Usuario>lista = new ArrayList<>();
+        String sql= "SELECT * FROM usuarios";
+
+        try (Connection conn = Conexion.conectar();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)){
+
+            while (rs.next()){
+                Usuario u = new Usuario(rs.getInt("id"),
+                                        rs.getString("nombre"),
+                                        rs.getString("email"));
+                lista.add(u);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public void actualizarUsuarios(Usuario usuario){
+        String sql = "UPDATE usuarios SET nombre=?, email=? WHERE id=?";
+
+        try(Connection conn = Conexion.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setString(1, usuario.getNombre());
+            stmt.setString(2, usuario.getEmail());
+            stmt.setInt(3, usuario.getId());
+            stmt.executeUpdate();
+            System.out.println("Usuario Actualizado.");
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void eliminarUsuario(int id){
+        String sql= "DELATE FROM usuarios WHERE id=?";
+
+        try(Connection conn = Conexion.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1,id);
+            stmt.executeUpdate();
+            System.out.println("Usuario eliminado.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
